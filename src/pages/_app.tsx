625e0f5bf391a5d472df4c2724c23c305812c9eb
Robin Lee <sl5035@columbia.edu>
2023-03-22 22:37:47 -0400

    favicon fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import '../styles/global.css';
 import Head from 'next/head';
+import favicon from '../../public/favicon.ico';
+import config from '../../config.json';
+
+type Props = {
+  title?: string;
+};
+
+const CustomHead = ({ title = config.title }: Props) => (
+  <Head>
+    <link rel="shortcut icon" href={favicon.src} type="image/x-icon" />
+    <meta
+      name="viewport"
+      content="initial-scale=1.0, width=device-width"
+      key="viewport"
+      maximum-scale="1"
+    />
+    <title>{title}</title>
+  </Head>
+);
 
 const App = ({ Component, pageProps }) => {
   const inputRef = React.useRef<HTMLInputElement>(null);
@@ -11,15 +30,7 @@ const App = ({ Component, pageProps }) => {
 
   return (
     <>
-      <Head>
-        <meta
-          name="viewport"
-          content="initial-scale=1.0, width=device-width"
-          key="viewport"
-          maximum-scale="1"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <CustomHead />
 
       <div
         className="text-light-foreground dark:text-dark-foreground min-w-max text-xs md:min-w-full md:text-base"
