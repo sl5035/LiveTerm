09390bac485450ad574fe07a5e89703765a594ed
Robin Lee <sl5035@columbia.edu>
2023-03-22 22:18:55 -0400

    sumfetch fix

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -1,23 +1,23 @@
 import config from '../../../config.json';
 
 const sumfetch = async (args: string[]): Promise<string> => {
-  if (config.ascii === 'cveinnt') {
+  if (Math.random() > 0.5) {
     return `                                                  
-             @@@@@@@@@@@@@                   sumfetch: summary display
+             @@@@@@@@@@@@@                   sumfetch
         @@@@               @@@@             -----------
       @@                       @@            ABOUT
     @@                           @@          ${config.name}
-  @@                               @@       ﰩ ${config.ps1_hostname}
- @@                         @@@     @@       <u><a href="${config.resume_url}" target="_blank">resume</a></u>
-@@        @@@                        @@     爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
-@@                                   @@     -----------
-@@             .@@@@@@@@@@.          @@      CONTACT 
- @@           @@          @@        @@       <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
-  @@           @@        @@        @@        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
-   @@             @@@@@@          @@         <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-     @@@                        @@@         -----------
-        @@@                  @@@ @@         
-
+  @@                               @@        <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+ @@                         @@@     @@      爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
+@@        @@@                        @@     -----------
+@@                                   @@      CONTACT
+@@             .@@@@@@@@@@.          @@      <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u> 
+ @@           @@          @@        @@       <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
+  @@           @@        @@        @@        <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
+   @@             @@@@@@          @@        -----------
+     @@@                        @@@         ⛿ PROJECTS 
+        @@@                  @@@ @@         ☞ <u><a href="https://github.com/sl5035/Space-Debris-Reentry target="_blank">${config.social.github}/Space-Debris-Reentry-Prediction</a></u>
+         @|  @@@@@@@@@@@@@@@@   @@          ☞ <u><a href="https://github.com/sl5035/3D-HisSite target="_blank">${config.social.github}/3d-HisSite</a></u>
 
 `;
   } else {
@@ -34,9 +34,9 @@ const sumfetch = async (args: string[]): Promise<string> => {
 ▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
    ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
     ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           -----------
-      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀             
-        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀               
-            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                  
+      ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀            ⛿ PROJECTS  
+        ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀               ☞ <u><a href="https://github.com/sl5035/Space-Debris-Reentry target="_blank">${config.social.github}/Space-Debris-Reentry-Prediction</a></u>
+            ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                  ☞ <u><a href="https://github.com/sl5035/3D-HisSite target="_blank">${config.social.github}/3d-HisSite</a></u>
 
 `;
   }
